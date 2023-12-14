0613b265a674cca67ae09c77d27795777d98446d
andrelosse <andre@planningservice.com.br>
2023-12-14 17:30:17 -0300

    Start implementation of Consulta and Cadastro Controllers

--- ClientManagement/Controllers/CadastroController.cs
+++ ClientManagement/Controllers/CadastroController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClientManagement.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClientManagement.Controllers
 {
     public class CadastroController : Controller
     {
+
+        private readonly DbContextConfig _CadastroDB;
+        private CadastroController(DbContextConfig CadastroDB) {
+            _CadastroDB = CadastroDB;
+        }
+        
         public IActionResult Index()
         {
             return View();
--- ClientManagement/Controllers/ConsultaController.cs
+++ ClientManagement/Controllers/ConsultaController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClientManagement.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClientManagement.Controllers
 {
     public class ConsultaController : Controller
     {
+        
+        private readonly DbContextConfig _ConsultaDB;
+        private ConsultaController(DbContextConfig ConsultaDB) {
+            _ConsultaDB = ConsultaDB;
+        }
+        
         public IActionResult Index()
         {
             return View();
